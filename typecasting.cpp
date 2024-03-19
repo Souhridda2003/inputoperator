4b42067efc7edef2b79e1a3718e941036f8dc544
Souhridda <99159450+Souhridda2003@users.noreply.github.com>
2024-03-19 16:59:58 +0530

    Add files via upload

---
+++ typecasting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // float x=7.1;
+    // int y;
+    // y=(int)x;
+    // cout<<y;
+    // bool x=true;
+    // cout<<x+9;
+    char ch = 'a';
+    int x=(int)ch;
+    cout<<x;
+}
